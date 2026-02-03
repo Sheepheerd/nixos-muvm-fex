ac5f83947605afad2264cdedb26fe407e5d4cb0d
Shepard Garrett <shepard@heerd.dev>
2026-02-03 15:35:33 -0500

    feat: trying to update more throughly

--- mesa.nix
+++ mesa.nix
@@ -9,12 +9,10 @@ let
   isCross = stdenv.hostPlatform != stdenv.buildPlatform;
 in
 mesa-asahi-edge.overrideAttrs (old: {
-  postInstall =
-    old.postInstall or ""
-    + ''
-      moveToOutput bin/vtn_bindgen2 $cross_tools
-      moveToOutput bin/asahi_clc $cross_tools
-    '';
+  postInstall = old.postInstall or "" + ''
+    moveToOutput bin/vtn_bindgen2 $cross_tools
+    moveToOutput bin/asahi_clc $cross_tools
+  '';
 
   LLVM_CONFIG_PATH = lib.optionalDrvAttr isCross "${llvm.dev}/bin/llvm-config-native";
 
@@ -23,4 +21,30 @@ mesa-asahi-edge.overrideAttrs (old: {
     ++ lib.optionals isCross [
       buildPackages.mesa-asahi-edge.cross_tools or null
     ];
+
+  # Make patch application more lenient - skip patches that fail to apply
+  # This is needed because some patches (like the rocket driver musl patch)
+  # may not apply if the target file doesn't exist in the Mesa version being used
+  patchPhase = ''
+    runHook prePatch
+    for patch in $patches; do
+      echo "Applying patch $patch"
+      # Try to apply patch in batch mode (non-interactive)
+      # If it fails, check if it's because the file doesn't exist
+      if patch --batch --forward --ignore-whitespace -p1 < "$patch" 2>&1; then
+        echo "Patch $patch applied successfully"
+      else
+        patch_exit=$?
+        # Exit code 1 in batch mode means patch couldn't be applied
+        # This is often because the target file doesn't exist
+        if [ $patch_exit -eq 1 ]; then
+          echo "Warning: Patch $patch failed to apply (file may not exist), skipping..."
+        else
+          echo "Error: Patch $patch failed with exit code $patch_exit"
+          exit $patch_exit
+        fi
+      fi
+    done
+    runHook postPatch
+  '';
 })
--- overlay.nix
+++ overlay.nix
@@ -8,14 +8,15 @@ let
       };
       mesonFlags = old.mesonFlags ++ [ (final.lib.mesonOption "drm-renderers" "asahi-experimental") ];
     });
+    mesa-asahi-edge = final.callPackage ./mesa.nix { inherit (prev) mesa-asahi-edge; };
     muvm = final.callPackage ./muvm.nix {
       inherit (prev) muvm;
-      mesa-x86_64-linux = final.pkgsCross.gnu64.mesa;
+      mesa-x86_64-linux = final.pkgsCross.gnu64.mesa-asahi-edge;
     };
-    fex = prev.fex;
+    fex = final.callPackage ./fex.nix { };
     fex-x86_64-rootfs = final.runCommand "fex-rootfs" { nativeBuildInputs = [ final.erofs-utils ]; } ''
       mkdir -p rootfs/run/opengl-driver
-      cp -R "${final.pkgsCross.gnu64.mesa}"/* rootfs/run/opengl-driver/
+      cp -R "${final.pkgsCross.gnu64.mesa-asahi-edge}"/* rootfs/run/opengl-driver/
       mkfs.erofs $out rootfs/
     '';
   };
@@ -26,10 +27,9 @@ let
 
   # Overlay which applies changes from https://github.com/NixOS/nixpkgs/pull/397932
   muvm-overlay = final: prev: {
-    libkrunfw = prev.libkrunfw;
-    libkrun = prev.libkrun;
-
-    muvm = prev.muvm;
+    libkrunfw = final.callPackage "${nixpkgs-muvm}/pkgs/by-name/li/libkrunfw/package.nix" { };
+    libkrun = final.callPackage "${nixpkgs-muvm}/pkgs/by-name/li/libkrun/package.nix" { };
+    muvm = final.callPackage "${nixpkgs-muvm}/pkgs/by-name/mu/muvm/package.nix" { };
   };
 
   overlays = [
